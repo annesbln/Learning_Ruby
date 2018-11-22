5eac5e4a711dec23eb01e23befed47838564b67d
annesbln <anne.schumann@outlook.com>
2018-11-22 21:52:53 +0100

    I developed my first guessing game and improved some minor things in previous exercises.

--- 2_ruby_monstas.rb
+++ 2_ruby_monstas.rb
@@ -1,6 +1,6 @@
 # In my very first lesson of the ruby monstas I learned that it is important to write as much code as possible:
 # Only via writing code, you actually learn how to code. So this is a small programm to show what I've learned
-# within the second three hour session. It is a small gessing game how to call certain formatting elements...
+# within the second three hour session. It is a small guessing game on how to call certain formatting elements...
 
 require 'io/console'
 
@@ -24,7 +24,6 @@ def play(hash)
   random_key = all_keys[rand(all_keys.size)]
   puts "How do you call these signs: #{random_key}?"
   answer = gets.chomp
-  puts "your answer is: #{answer}"
   if answer.downcase == hash[random_key].downcase
     puts "Yeah, you are right! Well done :)"
   else
